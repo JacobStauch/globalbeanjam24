9e7d82fc5d112f7d66591409fe5230e9a297b59a
Sarah-Salad <selester12@gmail.com>
2024-01-21 00:27:31 -0600

    Add starting dialogue

---
+++ assets/Scripts/DialogueBox.gd
@@ -0,0 +1,54 @@
+extends Node
+
+@onready var dialoguePath = "res://assets/Text/dialogue.json"
+@onready var textSpeed = 0.05
+
+var dialogue
+var phraseNum = 0
+var finished = false
+var currentState = "start"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.wait_time = textSpeed
+	dialogue = getDialogue()
+	nextPhrase()
+
+func getDialogue() -> Array:
+	var file = FileAccess.open(dialoguePath, FileAccess.READ)
+	var jsonContent = file.get_as_text()
+	var json = JSON.new()
+	var e = json.parse(jsonContent)
+	if e != OK:
+		print("Failed to load json: %s" % e)
+		return []
+	return json.data[currentState]
+
+
+func nextPhrase() -> void:
+	if phraseNum >= len(dialogue):
+		queue_free()
+		return
+	finished = false
+	
+	$Text.bbcode_text = "[color=black]" + dialogue[phraseNum]["text"]
+	
+	var img = "res://assets/Graphics/beanking" + "_" + dialogue[phraseNum]["emotion"] + ".png"
+	$Portrait.texture = load(img)
+	
+	$Text.visible_characters = 0
+	
+	while $Text.visible_characters < len($Text.text):
+		$Text.visible_characters += 1
+		
+		$Timer.start()
+		await $Timer.timeout
+	finished = true
+	phraseNum += 1
+	return
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if finished:
+			nextPhrase()
+		else:
+			$Text.visible_characters = len($Text.text)
