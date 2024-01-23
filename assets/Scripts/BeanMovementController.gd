09271f956af6eb15e3d4fd33ec8d48985d304e8a
Ben High <benhigh98@gmail.com>
2024-01-22 17:17:13 -0800

    Emit signal when bean is in last location and moves again

--- assets/Scripts/BeanMovementController.gd
+++ assets/Scripts/BeanMovementController.gd
@@ -5,6 +5,8 @@ extends Node2D
 @onready var root = $".."
 var i = 0
 
+signal at_end
+
 func _ready():
 	root.transform = path_manager.get_next_pos(-1)
 
@@ -12,7 +14,8 @@ func _on_movement_timer_timeout():
 	var next = path_manager.get_next_pos(i)
 	
 	if next == null:
-		timer.stop();
+		timer.stop()
+		at_end.emit()
 	else:
 		root.transform = next
 	i = i + 1
