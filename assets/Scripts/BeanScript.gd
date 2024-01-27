19a2d91799eadc56ef3cbbe0e103781c3ffb560c
Cameron Walker <cameronwalker218@gmail.com>
2024-01-27 16:02:00 -0600

    merge beanscript

--- assets/Scripts/BeanScript.gd
+++ assets/Scripts/BeanScript.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var promptHandler = $PromptHandler
 @onready var movementController = $MovementControl
 @onready var signalBus = get_node("/root/SignalBus")
+@onready var camera = %Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +19,13 @@ func _process(delta):
 func _on_prompt_done():
 	# Bubble up the prompt Done signal with the Bean node that triggered it
 	signalBus.beanPromptDoneSignal.emit(self)
+	var tween = get_tree().create_tween().set_parallel(true)
+	var cameraPosition = camera.get_screen_center_position()
+	var direction = 2000
+	if (position.x < cameraPosition.x):
+		direction = direction * -1
+	tween.tween_property(self, "position", Vector2(direction, -750), 1)
+	tween.tween_property(self, "rotation_degrees", 800.0, 1).as_relative()
 
 func _on_bean_selected():
 	signalBus.beanSelectedSignal.emit(self)
@@ -27,3 +35,4 @@ func _on_timeout():
 
 func get_bean_path_num():
 	return movementController.get_path_number()
+
