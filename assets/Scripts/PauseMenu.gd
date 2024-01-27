14fb2f9bf79ecb620a390f09af3beb50119607a7
Ben High <benhigh98@gmail.com>
2024-01-26 18:46:29 -0800

    Allow pause with escape and make pause menu buttons do things

--- assets/Scripts/PauseMenu.gd
+++ assets/Scripts/PauseMenu.gd
@@ -1,10 +1,21 @@
 extends Control
 
-signal quit
-signal resume
+@onready var this = $"."
+
+func togglePause():
+	get_tree().paused = !get_tree().paused
+	if get_tree().paused:
+		this.show()
+	else:
+		this.hide()
 
 func _on_resume_button_pressed():
-	resume.emit()
+	togglePause()
 
 func _on_quit_button_pressed():
-	quit.emit()
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://assets/Scenes/Objects/MainMenu.tscn")
+
+func _input(event):
+	if event.is_action_pressed("Pause"):
+		togglePause()
