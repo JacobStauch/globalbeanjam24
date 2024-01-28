2a2cd7e58b2fd55dc485f5ea59434b7c10890e03
Sarah-Salad <selester12@gmail.com>
2024-01-28 16:17:37 -0600

    update level times

--- assets/Scripts/GameManager.gd
+++ assets/Scripts/GameManager.gd
@@ -32,7 +32,7 @@ extends Node2D
 @onready var curLevel = levels[curLevelIndex]
 
 #Create level timer vars
-@onready var levelDurations = [4, 4, 30]
+@onready var levelDurations = [40, 40, 30]
 @onready var curLevelDuration = 0
 @onready var levelStopwatch := 0.0
 
